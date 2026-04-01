386eec0a3b6d26d757cc09ca164bc608817bfcb9
Nam Tran <nam.tp@dgwasia.com>
2026-04-01 11:30:21 +0700

    add tagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,29 @@ export default function RootLayout({
   return (
     <html lang="vi" className="scroll-smooth">
       <body className={`${inter.className} min-h-screen flex flex-col pt-16`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P28HSC8M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P28HSC8M');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         <Header />
         <main className="flex-1">
           {children}
